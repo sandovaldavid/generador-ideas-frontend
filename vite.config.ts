25395a01ce3d1cf28815cd9db39a7e7ff13f0e95
Juan David Sandoval Salvador <102970701+sandovaldavid@users.noreply.github.com>
2025-10-16 22:54:06 -0500

    🎉 Initialize project structure with essential configuration files and assets

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@config": path.resolve(__dirname, "src/config"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
